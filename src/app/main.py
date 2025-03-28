c6bc5b1adfc4f29b7b3388bb0c3835baf34fc1cc
Henry Shaw <henryshaw747@gmail.com>
2025-03-28 15:50:41 -0700

    add architecture comment in entry point

--- src/app/main.py
+++ src/app/main.py
@@ -6,3 +6,18 @@ version: 0.1
 
 # prototype application entry point:
 print("Bee Counting Tool: (TO BE IMPLEMENTED)")
+'''
+# for program loop:
+1. Have user select or use new spreadsheet to work out of. 
+(this is actually easier with CLI since they should already be in the working directory.)
+2. Await user input action:
+    a. Manually enter a bee sample.
+    b. Process a bee sample from image file.
+    c. Process a bee sample from webcam.
+    d. Exit, save and close. (or variations of this)
+3. Execute selection action, return to step 2.
+'''
+
+'''
+Strategies for error hanlding:
+'''
