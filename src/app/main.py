ab86dcb90f2b8a7b391bb9e68b0831fcf29c61e0
Henry Shaw <henryshaw747@gmail.com>
2025-03-27 18:46:26 -0700

    add application entry point

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,8 @@
 '''
-Let this be the entry point for the prototype application.
+Project Thames Bee Counting Tool
+authors: Connor Clouse, John Pratt, Henry Shaw
+version: 0.1
 '''
 
+# prototype application entry point:
 print("Bee Counting Tool: (TO BE IMPLEMENTED)")
