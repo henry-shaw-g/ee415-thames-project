aeee305072b32113c2c01cf2c7bbdcc4ccba7a34
Henry Shaw <henryshaw747@gmail.com>
2025-04-08 11:37:03 -0700

    implement camera feed display if needed

--- src/app/frontend_display.py
+++ src/app/frontend_display.py
@@ -29,12 +29,15 @@ class FrontendDisplay:
     def __init__(self):
         self.main = None
         self._q = Queue()
+        self._livecam_q = Queue(maxsize=1)
+        self._img_ptr_readonly = None
 
+        self._image_view_size = (800, 600)
 
         # create Tk gui instances
         self.root = tk.Tk()
         self.root.title("Bee Count Display")
-        self.root.geometry("1000x1000")
+        self.root.geometry(f"{self._image_view_size[0]}x{self._image_view_size[1]}")
 
         self.style = ttk.Style(self.root)
         self.style.theme_use("classic")
@@ -48,7 +51,7 @@ class FrontendDisplay:
         self.empty_frame = ttk.Frame(self.top_frame)
         self.empty_frame.grid(row=0, column=0, sticky=tk.NSEW)
         self.empty_label = ttk.Label(self.empty_frame, text="Awaiting input...")
-        self.empty_label.pack(anchor=tk.CENTER, side=tk.TOP)
+        self.empty_label.pack(anchor=tk.CENTER, side=tk.TOP, fill=tk.Y, expand=True)
         # create a canvas to display images
 
         self.result_frame = ttk.Frame(self.top_frame)
@@ -68,8 +71,9 @@ class FrontendDisplay:
         self.result_label_count.pack(anchor=tk.N, side=tk.TOP)
         
         # create a matlab figure canvas for image display, hook up input events for navigation
-        fig = Figure(figsize=(6, 4), dpi= 100)
+        fig = Figure(figsize=(self._image_view_size[0] / 100, self._image_view_size[1] / 100), dpi=100)
         ax = fig.add_axes([0, 0, 1, 1])
+        ax_im = ax.imshow(np.zeros((1080, 1920, 3), dtype=np.uint8))
         ax.axis("off")
         # ax.imshow(np.zeros((1, 1, 3), dtype=np.uint8))
         canvas = FigureCanvasTkAgg(fig, master=self.result_frame_left)
@@ -86,6 +90,7 @@ class FrontendDisplay:
         self.result_toolbar = toolbar
         self.result_fig = fig
         self.result_ax = ax
+        self.result_ax_im = ax_im
 
         self.result_frame.tkraise()
 
@@ -104,7 +109,17 @@ class FrontendDisplay:
         self.root.mainloop()
 
     def _queue_loop(self):
-        
+        try:
+            msg = self._livecam_q.get_nowait()
+            kind, *data = msg
+            if kind == "livecamera":
+                self._on_live_camera_result(*data)
+        except queue.Empty:
+            pass
+        except Exception as e:
+            print("Error in frontend_display live cam queue loop.")
+            raise
+
         try:
             msg = self._q.get_nowait()
             kind, *data = msg
@@ -112,8 +127,6 @@ class FrontendDisplay:
                 self._on_empty()
             elif kind == "static_result":
                 self._on_static_result(*data)
-            elif kind == "livecamera":
-                pass
         
         except queue.Empty:
             # end of queue
@@ -125,31 +138,35 @@ class FrontendDisplay:
         self.root.after(QUEUE_CHECK_PERIOD, self._queue_loop)
     
     def push_empty(self):
-        self._q.put(("empty"))
+        self._q.put(("empty",))
 
     '''
     Send messaage to display to show result of static image bee counting.
     output_img: BGR openCV image
     '''
     def push_static_result(self, output_img: cv.typing.MatLike):
-        print("pushing static result")
         self._q.put(("static_result", output_img))
 
     '''
     Send data update containing live camera image, etc.
     '''
     def push_live_camera_result(self, output_img: cv.typing.MatLike):
-        pass
+        self._img_ptr_readonly = output_img
+        try:
+            self._livecam_q.put_nowait(("livecamera",))
+        except:
+            pass # queue is full, ignore
 
     def _on_empty(self):
+        print("on empty")
         self.empty_frame.tkraise()
         self.empty_label.update()
         self.empty_frame.update()
         self.result_frame.lower()
 
     def _on_static_result(self, output_img):
-        print("on static result")
         matplot_img = cv.cvtColor(output_img, cv.COLOR_BGR2RGB)
+        self.result_ax_im.set_data(matplot_img)
         # self.result_ax.clear()
         # self.result_ax.axis("off")
         self.result_ax.imshow(matplot_img)
@@ -158,9 +175,31 @@ class FrontendDisplay:
         self.result_frame.update()
         self.result_frame.tkraise()
 
-if __name__ == "__main__":
-    # This is for testing the FrontendDisplay class
+    def _on_live_camera_result(self):
+        start = time.time_ns()
+        
+        output_img = self._img_ptr_readonly
 
+        # matplot_img = cv.cvtColor(output_img, cv.COLOR_BGR2RGB)
+        s1 = time.time_ns()
+        matplot_img = output_img[:,:,::-1]
+        t1 = time.time_ns() - s1
+        s2 = time.time_ns()
+        self._display_img_data(matplot_img)
+        t2 = time.time_ns() - s2
+        self.result_frame.tkraise()
+        print("live camera result processing times: ", t1, t2)
+
+        '''
+        img_data: RGB channel image
+        '''
+    def _display_img_data(self, img_data):
+        self.result_ax_im.set_data(img_data)
+        self.result_canvas.draw()
+        self.result_frame.update()
+
+# tests
+def _test_frontend_display_window():
     def main(display: FrontendDisplay):
         img = cv.imread("images/bee-image-samples/bee-image-1.jpg")
         
@@ -173,3 +212,33 @@ if __name__ == "__main__":
     display.main = main
     display.start()
     print("Bee Count Tool: exiting ...")
+
+def _test_frontend_display_live():
+    cam = cv.VideoCapture(1) # 1 for macos, 0 for other systems
+    if not cam.isOpened():
+        cam.open()
+    
+    def main(display: FrontendDisplay):
+        while True:
+            ret, frame = cam.read()
+            if not ret:
+                break
+
+            print("sending frame")
+            display.push_live_camera_result(frame)
+            time.sleep(0.05)
+
+    display = FrontendDisplay()
+    display.main = main
+    display.start()
+
+def _test_frontend_display_empty():
+    def main(display: FrontendDisplay):
+        display.push_empty()
+
+    display = FrontendDisplay()
+    display.main = main
+    display.start()
+
+if __name__ == "__main__":
+    _test_frontend_display_live()
