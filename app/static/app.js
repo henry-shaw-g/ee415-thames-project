641b9944946202bc6557de6247aaf3c3b6c47ed4
Henry <henrygordonshaw@gmail.com>
2025-08-28 11:39:58 -0700

    add browser webcam demo

--- app/static/app.js
+++ app/static/app.js
@@ -1,3 +1,23 @@
 function showMessage() {
     document.getElementById('message').textContent = 'Hello from Flask Single Page App!';
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    let video = document.getElementById("vid");
+    let mediaDevices = navigator.mediaDevices;
+    vid.muted = true;
+    // Accessing the user camera and video.
+    mediaDevices
+        .getUserMedia({
+            video: true,
+            audio: true,
+        })
+        .then((stream) => {
+            // Changing the source of video to current stream.
+            video.srcObject = stream;
+            video.addEventListener("loadedmetadata", () => {
+                video.play();
+            });
+        })
+        .catch(alert);
+});
