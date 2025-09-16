296354d50d68e2d45201961d7a58f4eed46d34a5
Connor Clouse <connor.clouse@wsu.edu>
2025-09-15 18:22:47 -0700

    see last commit message

--- counting/algorithm.py
+++ counting/algorithm.py
@@ -1,14 +1,19 @@
 import contour
 import image
+import json
+import pathlib
 
 
-#inputs: Image, settings file (path? or dict?)
+#inputs: Image, settings file path
 #outputs: Bee count, image with contours to display on frontend, 
 #debug: list of contours in python memory 
 
-def algorithm():
+def algorithm(image_path, settings_path):
     
     #Put image.png into image class instance
+    settings = get_settings(settings_path)
+
+    image = image.Image(image_path, settings)
 
 
     #do manipulations on image using class members
@@ -21,11 +26,24 @@ def algorithm():
     pass
     
 
+def get_settings(settings_path):
+    #read settings file
+    if settings_path is None:
+        settings_path = "default_settings.json" 
+
+    with open(settings_path, 'r') as f:
+        settings = json.load(f)
+    
+    return settings
+
 
 
 if __name__ == "__main__":
-    #algorithm called as a script
-    #
+    settings_path = None
+    image_path = None
+
+    algorithm(image_path, settings_path)
+
     pass
 
 
--- counting/contour.py
+++ counting/contour.py
@@ -1,2 +1,15 @@
-# Class with member variable contour and functions to manipulate contours  
+import cv2 as cv
 
+class Coutour:
+    def __init__(self, image_thresholded, settings_counting):
+        self.image_thresholded = image_thresholded
+        self.settings_counting = settings_counting
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    pass
