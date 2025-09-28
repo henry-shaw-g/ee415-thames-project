24a0dffa1c9a5355a2a9f2af7555a1c121d0b7b1
Connor Clouse <connor.clouse@wsu.edu>
2025-09-28 16:31:01 -0700

    finished find_countours and added structure to Contours class

--- counting/contours.py
+++ counting/contours.py
@@ -1,14 +1,15 @@
 import cv2 as cv
 
-class Coutour:
+class Coutours:
     def __init__(self, image_thresholded, settings_counting):
         self.image_thresholded = image_thresholded
         self.settings_counting = settings_counting
 
+        self.contours = []
+        self.hierarchy = None
+
     def find_contours(self):
-
         contours, hierarchy = cv.findContours(self.image_thresholded, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
 
         self.contours = contours
         self.hierarchy = hierarchy
