707758dbf7b782a380d08c2df1c3f4dc5056a619
John Pratt <john.pratt@wsu.edu>
2025-09-28 16:33:06 -0700

    readded ui folders and changed main a bit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+from user_interface.frontend_display import FrontendDisplay
 def main():
     #call frontend
     pass
@@ -6,3 +7,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+    counting_settings = None #PUT FILEPATH HERE EMPTY FOR NOW WILL BE NORMAL SETTINGS
+    default_settings = None #ALSO PUT FILEPATH HERE FOR DEFAULT SETTINGS
+    display = FrontendDisplay(counting_settings=counting_settings)
+    display.start()
