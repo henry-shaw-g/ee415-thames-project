534f92fe281b5e7a6e150337667a624dc293bfa5
John Pratt <john.pratt@wsu.edu>
2025-09-28 16:50:51 -0700

    adds to alg_settings

--- user_interface/alg_settings.py
+++ user_interface/alg_settings.py
@@ -9,30 +9,47 @@ import pandas as pd
 import tkinter.ttk as ttk
 
 class AlgSettings :
-    def __init__(self,defaultpath,normalpath):
+    def __init__(self,defaultpath,normalpath,rootWindow):
 
         # JSON file path variables (default only read never edit)
         self.DefaultSettingsFilepath = defaultpath
         self.NormalSettingsFilepath = normalpath
 
-        # Entry variables
+        # data variables
+        self.blur_ksize = [0,0]
+        self.threshold_value = 0
+        self.threshold_maxval = 0
+        self.min_contour_area = 0
+        self.max_contour_area = 0
 
         # frame variables
-        self.root = tk.Tk()
-        self.root.title('Algorithm Settings')
-        self.root.geometry("400x800")
+        self.Top = tk.Toplevel(rootWindow)
+        self.Top.title('Algorithm Settings')
+        self.Top.geometry("400x800")
         # entry variables
-       
+
         # button variables
 
+
+    #can expand overtime, not sure if it is needed
+    def UpdateValues(self, newBlurKSize, newThresholdValue, newThresholdMaxVal, newMinContArea, newMaxContArea):
+        self.blur_ksize = newBlurKSize
+        self.threshold_value = newThresholdValue
+        self.threshold_maxval = newThresholdMaxVal
+        self.min_contour_area = newMinContArea
+        self.max_contour_area = newMaxContArea
+
     #reads settings file to local variables
-    #def readSettings(self)
-        
+    def readSettings(self):
 
-    # writes new entries into normal file
-    #def writeSettings(self)
-        
+        if self.DefaultSettingsFilepath is None:
+            print("Error: No default settings file found")
+        else:
+            print("Getting Algorithm Settings")
 
-    # resets normal file to default settings
-    #def resetSettings(self)
-        
+
+    def writeSettings(self):
+        if self.DefaultSettingsFilepath and self.NormalSettingsFilepath is not None:
+            print("Updating Settings")
+        else:
+            print("Error: No filepaths detected")
