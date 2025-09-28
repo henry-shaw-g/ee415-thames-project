2a1c1895dca26224053d5b4c033e1409adf69330
John Pratt <john.pratt@wsu.edu>
2025-09-28 15:53:25 -0700

    adds new json settings

--- user interface/frontend_display.py
+++ user interface/frontend_display.py
@@ -5,7 +5,7 @@ date: 2025-09-28
 notes:
 
 '''
-#dependancies
+# library dependancies
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import filedialog
@@ -16,4 +16,17 @@ import numpy as np
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.figure import Figure
-#files to import
+# files to import from
+from data_io import DataIO
+from alg_settings import AlgSettings
+
+
+
+
+class FrontendDisplay:
+
+    def __init__(self, counting_settings,default_counting_settings):
+        #filepaths for both the normal and default counting setting JSON files
+        self.counting_settings = counting_settings
+        self.defualt_counting_settings = default_counting_settings
+
