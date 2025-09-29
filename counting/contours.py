23b800a516d44e2e9dffd94aae1c10c765212a50
Connor Clouse <connor.clouse@wsu.edu>
2025-09-28 17:38:07 -0700

    Changed Structure of Contours.py. Now member contours will be an array of contour objects

--- counting/contours.py
+++ counting/contours.py
@@ -1,21 +1,33 @@
 import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
 
-class Coutours:
+from counting.contour import Contour
+
+class Contours:
     def __init__(self, image_thresholded, settings_counting):
         self.image_thresholded = image_thresholded
         self.settings_counting = settings_counting
 
-        self.contours = []
-        self.hierarchy = None
+        self._find_countours_contours = None 
+        self._find_countours_hierarchy = None
+
+        self.contours = []  # list of Contour objects
+
 
     def find_contours(self):
         contours, hierarchy = cv.findContours(self.image_thresholded, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-        self.contours = contours
-        self.hierarchy = hierarchy
+        self._find_countours_contours = contours
+        self._find_countours_hierarchy = hierarchy
+
+        for cnt in contours:
+            contour_obj = Contour(cnt, hierarchy)
+            self.contours.append(contour_obj)
+
         return contours, hierarchy
 
-
+      
 
 
 if __name__ == "__main__":
