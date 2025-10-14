43c413892cef022c33f00a29b9d5feb697b7f4d4
Henry Shaw <henryshaw747@gmail.com>
2025-10-14 00:52:57 -0700

    fix contour merge logic errors

--- counting/contour_merge.py
+++ counting/contour_merge.py
@@ -16,33 +16,34 @@ def _get_bbox_dimension_cells(cells_dim, cell_size, contour):
     bx, by = contour.bounding_box_x, contour.bounding_box_y
     bw, bh = contour.bounding_box_w, contour.bounding_box_h
 
-    cell_j0 = max(0, math.floor(by / cell_size))
-    cell_j1 = min(cells_dim[0] - 1, math.floor((by + bh) / cell_size))
-    cell_i0 = max(0, math.floor(bx / cell_size))
-    cell_i1 = min(cells_dim[1] - 1, math.floor((bx + bw) / cell_size))
+    # i denotes row (y), j denotes column (x)
+    cell_i0 = max(0, math.floor(by / cell_size))
+    cell_i1 = min(cells_dim[1] - 1, math.floor((by + bh) / cell_size))
+    cell_j0 = max(0, math.floor(bx / cell_size))
+    cell_j1 = min(cells_dim[0] - 1, math.floor((bx + bw) / cell_size))
 
     return range(cell_j0, cell_j1 + 1), range(cell_i0, cell_i1 + 1)
 
 def _init_grid(image_w, image_h):
     cells_wide = math.ceil(image_w / GRID_SIZE)
     cells_high = math.ceil(image_h/ GRID_SIZE)
-    grid = [([] for j in range(0, cells_wide)) for i in range(0, cells_high)]
+    grid = [[[] for j in range(0, cells_wide)] for i in range(0, cells_high)]
     return grid, (cells_wide, cells_high)
 
 
-def _populate_grid(grid, contours):
+def _populate_grid(grid, grid_dims, contours):
     for contour in contours:
-        cells_y, cells_x = _get_bbox_dimension_cells((len(grid), len(grid[0])), GRID_SIZE, contour)
+        cells_x, cells_y = _get_bbox_dimension_cells((grid_dims[0], grid_dims[1]), GRID_SIZE, contour)
         for i in cells_y:
             for j in cells_x:
                 grid[i][j].append(contour)
 
 class Merger:
-    def __init__(self, image, contours, settings):
+    def __init__(self, image, contours, algorithm_settings):
         self.contours = sorted(contours, key=lambda c: c.area, reverse=True)
-        self.settings = settings
+        self.settings = algorithm_settings
         self.grid, self.grid_dims = _init_grid(image.shape[1], image.shape[0])
-        _populate_grid(grid, contours)
+        _populate_grid(self.grid, self.grid_dims, contours)
 
 
     def _merge(self):
@@ -57,12 +58,14 @@ class Merger:
             cells_y, cells_x = _get_bbox_dimension_cells(self.grid_dims, GRID_SIZE, contour)
             for i in cells_y:
                 for j in cells_x:
-                    for other in grid[i][j]:
+                    for other in self.grid[i][j]:
                         if other is contour:
                             continue
 
                         if other.get_type() is Contour.type.single_bee:
-                            if self._check_single_mergeable(other, contour):
+                            (mergeable, IOU, intersection) = self._check_single_mergeable(other, contour)
+                            if mergeable:
+                                print(f"merged contour id={contour.id} into other id={other.id} in cell (i={i},j={j}). {IOU=}, {intersection=}")
                                 contour.set_type(Contour.type.rejected)
                                 break
 
@@ -90,11 +93,11 @@ class Merger:
         angle2 = abs((bigger.fitted_ellipse_angle - 180) % 360 - smaller.fitted_ellipse_angle)
         angle_diff = min(angle1, angle2)
         if angle_diff > self.settings["contour_merge_angle_threshold"]:
-            return False
+            return False, None, None
         
         (intersection, _) = cv.intersectConvexConvex(bigger.contour, smaller.contour)
-        IOU = intersection / (bigger.area + smaller.area - intersection[0])
-        return IOU < self.settings["contour_merge_IOU_threshold"]
+        IOU = intersection / (bigger.area + smaller.area - intersection)
+        return (IOU > self.settings["contour_merge_IOU_threshold"], IOU, intersection)
 
     '''
     fn: check_single_in_clump
