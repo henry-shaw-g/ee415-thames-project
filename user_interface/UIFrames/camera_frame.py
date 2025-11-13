82762b882cd31cdfcc1776b60f5f4d1fdfb9337a
Henry Shaw <henryshaw747@gmail.com>
2025-11-13 11:35:31 -0800

    add more small camera fixes

--- user_interface/UIFrames/camera_frame.py
+++ user_interface/UIFrames/camera_frame.py
@@ -2,8 +2,22 @@ import tkinter as tk
 import cv2 as cv
 from PIL import Image
 from PIL import ImageTk
+from cv2_enumerate_cameras import enumerate_cameras
 
 DEVICE_NAME = "UVC Camera"                 # exact name from Device Manager
+
+def find_camera_index(device_name=DEVICE_NAME):
+    print("Enumerating cameras...")
+    cameras = enumerate_cameras()
+    for camera_info in cameras:
+        index = camera_info.index
+        name = camera_info.name
+        print("Found camera:", index, name)
+        if name == device_name:
+            print("Using camera index:", index)
+            return index
+    return None
+
 
 class CameraFrame(tk.Frame):
 
@@ -11,8 +25,8 @@ class CameraFrame(tk.Frame):
         self.controller = controller
         tk.Frame.__init__(self, parent)
         #TODO add script here (testing with my webcam rn)
-        self.cameraIndexNum = 1
-        self.capture = cv.VideoCapture(self.cameraIndexNum, cv.CAP_DSHOW) #Get the correct camera
+        self.cameraIndexNum = find_camera_index(device_name=DEVICE_NAME)
+        self.capture = cv.VideoCapture(self.cameraIndexNum) #Get the correct camera
         if self.capture.isOpened() is not True:
             print("Error: Could not find correct camera. Using default camera")
             self.capture = cv.VideoCapture(0)  #If we can't find the correct device name then we just use the default
@@ -51,7 +65,7 @@ class CameraFrame(tk.Frame):
         self.after(100,self.showCameraFrame)
 
     def checkCamera(self):
-        self.capture = cv.VideoCapture(DEVICE_NAME) #Try to get the correct camera again
+        self.capture = cv.VideoCapture(self.cameraIndexNum) #Try to get the correct camera again
         if self.capture.isOpened() is not True:
             print("Error: Could not find correct camera. Using default camera")
             self.capture = cv.VideoCapture(0)  #If we can't find the correct device name then we just use the default again
@@ -60,31 +74,31 @@ class CameraFrame(tk.Frame):
         #assuming self.capture is not none so we just have access to it
         if self.imgtk is not None: #check to see if camera is running
             print("Taking photo")
-            self.capturePhoto = cv.VideoCapture(self.cameraIndexNum, cv.CAP_DSHOW)
-            if self.capturePhoto.isOpened(): # not sure if I need this but we run it
+            # self.capturePhoto = cv.VideoCapture(self.cameraIndexNum, cv.CAP_DSHOW)
+            # if self.capturePhoto.isOpened(): # not sure if I need this but we run it
                 #set new resolution for 4k picture
-                self.capture.set(cv.CAP_PROP_FRAME_WIDTH, 3840) #4k width
-                self.capture.set(cv.CAP_PROP_FRAME_HEIGHT, 2160) #4k height
-                
-                width = self.capture.get(cv.CAP_PROP_FRAME_WIDTH)
-                height = self.capture.get(cv.CAP_PROP_FRAME_HEIGHT)
-                print(f"width: {width}, heigth: {height}") #check to make sure its printing the image at 4k
+            self.capture.set(cv.CAP_PROP_FRAME_WIDTH, 3840) #4k width
+            self.capture.set(cv.CAP_PROP_FRAME_HEIGHT, 2160) #4k height
+            
+            width = self.capture.get(cv.CAP_PROP_FRAME_WIDTH)
+            height = self.capture.get(cv.CAP_PROP_FRAME_HEIGHT)
+            print(f"width: {width}, heigth: {height}") #check to make sure its printing the image at 4k
 
-                ret,frame = self.capturePhoto.read() #actually get the photo
-                if ret:
-                    #reset capture size
-                    self.capture.set(cv.CAP_PROP_FRAME_WIDTH, 800)
-                    self.capture.set(cv.CAP_PROP_FRAME_HEIGHT, 800) 
+            ret,frame = self.capture.read() #actually get the photo
+            if ret:
+                #reset capture size
+                self.capture.set(cv.CAP_PROP_FRAME_WIDTH, 800)
+                self.capture.set(cv.CAP_PROP_FRAME_HEIGHT, 800) 
 
-                    return frame #return image through function only if the capture worked
-                else:
-                    #reset capture size
-                    self.capture.set(cv.CAP_PROP_FRAME_WIDTH, 800)
-                    self.capture.set(cv.CAP_PROP_FRAME_HEIGHT, 800) 
-                    return None
+                return frame #return image through function only if the capture worked
             else:
-                print("Error: Wrong Camera attatched, please attach correct camera")
+                #reset capture size
+                self.capture.set(cv.CAP_PROP_FRAME_WIDTH, 800)
+                self.capture.set(cv.CAP_PROP_FRAME_HEIGHT, 800) 
                 return None
+            # else:
+            #     print("Error: Wrong Camera attatched, please attach correct camera")
+            #     return None
         else:
             print("Error: No camera running")
             return None
